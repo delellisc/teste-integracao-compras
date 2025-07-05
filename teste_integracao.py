382d3d02b66826e0b1e2abdd2a4a0a2b49c41b0c
delellisc <delellis021@gmail.com>
2025-07-04 23:48:14 -0300

    Writing list user purchases and list a user without purchases tests.

--- teste_integracao.py
+++ teste_integracao.py
@@ -14,12 +14,10 @@ def test_fluxo_integra_compras():
     # 1. Cadastrar usuários e testar
     cadastrar_usuario(1, "João de Deus")
     assert consultar_usuario(1) == "João de Deus", "Usuário não foi cadastrado corretamente."
-    # assert usuarios_db == {1: "João de Deus"}, "Usuário não foi cadastrado corretamente."
 
     # 2. Cadastrar produtos e testar    
     cadastrar_produto(1, "Prancha em Miniatura", 100)
     assert consultar_produto(1) == {'nome': 'Prancha em Miniatura', 'preco': 100}, "Produto não condizente."
-    
     
     # 3. Realizar compra válida
     realizar_compra(1, [1])
@@ -30,12 +28,14 @@ def test_fluxo_integra_compras():
         realizar_compra(1, [2])
         assert False, "Esperado erro ao comprar produto inexistente"
     except ValueError as error_msg:
-        assert str(error_msg) == "Produto 2 não encontrado.", f"Mensagem de erro incorreta: {e}"
+        assert str(error_msg) == "Produto 2 não encontrado.", f"Mensagem de erro incorreta: {error_msg}"
 
     # 5. Verificar compras do usuário 1
-    
+    assert listar_compras(1) == [{'produtos': [1], 'total': 100}]
 
     # 6. Verificar um usuário que não tem compras
+    cadastrar_usuario(2, "Maria José")
+    assert listar_compras(2) == []
 
 # Executa o teste
 if __name__ == "__main__":
